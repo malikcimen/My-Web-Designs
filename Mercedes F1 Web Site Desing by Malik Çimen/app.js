04f2927fd9d83aba91cdd2ffb1680aeb24f5f73b
Malik Çimen <malikcimen@hotmail.com>
2022-01-02 13:38:47 +0300

    Designed by Malik Çimen

---
+++ Mercedes F1 Web Site Desing by Malik Çimen/app.js
@@ -0,0 +1,24 @@
+// Setting up the Variables
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
+
+
+//setting up the listener
+bars.addEventListener("click", barClicked, false);
+
+
+//setting up the clicked Effect
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
+
+
+$(".slider").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000, //2000ms = 2s;
+  autoplayHoverPause: true,
+});
+
+
